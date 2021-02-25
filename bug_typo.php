03ed962d42302243893023c296d60ac12b19645d
Eriam Schaffter <eriam@mediavirtuel.com>
2021-02-25 04:55:05 +0100

    Create bug_typo.php

---
+++ bug_typo.php
@@ -0,0 +1,7 @@
+<?php
+// Ce script devrait afficher Hello World 
+// suivi du nom associé au paramètre $_GET
+// nom mais il ne fonctionne pas de la 
+// manière attendue ..
+echo "Hello World ".$argv[0];
+?>
